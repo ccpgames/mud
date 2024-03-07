85982da98692f998584cf814e68f4f1921f1a597
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-07 15:41:59 +0000

    scope + schema implementation

--- packages/store/ts/config/v2/schema.test.ts
+++ packages/store/ts/config/v2/schema.test.ts
@@ -17,13 +17,20 @@ describe("resolveSchema", () => {
         internalType: "CustomType",
       },
     } as const;
-    attest<typeof expected>(resolved).type.toString.snap(
-      '{ regular: { type: "uint256"; internalType: "uint256"; }; user: { type: "address"; internalType: "CustomType"; }; }',
-    );
+
+    attest<typeof expected>(resolved)
+      .snap({
+        regular: { type: "uint256", internalType: "uint256" },
+        user: { type: "address", internalType: "CustomType" },
+      })
+      .type.toString.snap(
+        '{ regular: { type: "uint256"; internalType: "uint256"; }; user: { type: "address"; internalType: "CustomType"; }; }',
+      );
   });
 
   it("should throw if a type is not part of the scope", () => {
     const scope = extendScope(AbiTypeScope, { CustomType: "address" });
+    // TODO: test that this fails runtime validation
     resolveSchema(
       {
         regular: "uint256",
